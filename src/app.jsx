1966d3ecb406cca853060bb9a3c03802d662cff2
most4f4 <mhasanalipourshahrab@myseneca.ca>
2025-05-12 19:51:53 -0400

    the inner container component created

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
 import "./app.css";
 import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
 
 export function App() {
   const [foodData, setFoodData] = useState([]);
@@ -12,7 +13,9 @@ export function App() {
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <Container>
-        <FoodList foodData={foodData} />
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
       </Container>
     </div>
   );
