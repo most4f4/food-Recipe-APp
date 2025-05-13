57badadd6ee96a3557dd45497abc53b974a51e37
most4f4 <mhasanalipourshahrab@myseneca.ca>
2025-05-12 21:22:38 -0400

    the recipe detail component is designed

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from "./fooddetails.module.css";
 
 export default function FoodDetails({ foodId }) {
   const [food, setFood] = useState({});
@@ -20,31 +21,41 @@ export default function FoodDetails({ foodId }) {
 
   return (
     <div>
-      <div>
-        <h1>{food.title}</h1>
-        <img src={food.image} alt="" />
-        <div>
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeName}>{food.title}</h1>
+        <img className={styles.recipeImage} src={food.image} alt="" />
+        <div className={styles.recipeDetails}>
           <span>
             <strong>⏰ {food.readyInMinutes} Minutes</strong>
           </span>
           <span>
             <strong>👨‍👩‍👧‍👦 Serves {food.servings}</strong>
           </span>
-          <span>{food.vegetarian ? "🥕 Vegetarian" : "🍖 Non-Vegetarian"}</span>
-          <span>{food.vegan ? "🤠 Vegan" : ""}</span>
+          <span>
+            <strong>
+              {food.vegetarian ? "🥕 Vegetarian" : "🍖 Non-Vegetarian"}
+            </strong>
+          </span>
+          <span>
+            <strong>{food.vegan ? "🤠 Vegan" : ""}</strong>
+          </span>
         </div>
         <div>
           $ <span>{(food.pricePerServing / 100).toFixed(2)} per serving</span>
         </div>
-      </div>
 
-      <div>
         <h2>Instructions</h2>
-        {isLoading ? (
-          <p>Loading ....</p>
-        ) : (
-          food.analyzedInstructions[0].steps.map((step) => <li>{step.step}</li>)
-        )}
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {isLoading ? (
+              <p>Loading ....</p>
+            ) : (
+              food.analyzedInstructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
       </div>
     </div>
   );
