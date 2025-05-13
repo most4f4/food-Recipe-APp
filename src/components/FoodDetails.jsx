611c400dd774410e6224ef09442ad8f1f5500c75
most4f4 <mhasanalipourshahrab@myseneca.ca>
2025-05-12 21:36:21 -0400

    ingredients for a recipe along with their images and quantities are fetched and displayed

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -44,6 +44,27 @@ export default function FoodDetails({ foodId }) {
           $ <span>{(food.pricePerServing / 100).toFixed(2)} per serving</span>
         </div>
 
+        <h2>Ingredients</h2>
+        {isLoading ? (
+          <p>Loading ....</p>
+        ) : (
+          food.extendedIngredients.map((item) => (
+            <div>
+              <img
+                src={
+                  `https://spoonacular.com/cdn/ingredients_100x100/` +
+                  item.image
+                }
+                alt=""
+              />
+              <h3>{item.name}</h3>
+              <h3>
+                {item.amount} {item.unit}
+              </h3>
+            </div>
+          ))
+        )}
+
         <h2>Instructions</h2>
         <div className={styles.recipeInstructions}>
           <ol>
